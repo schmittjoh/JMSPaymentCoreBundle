daabf7b2cf9da4f0a7be2a2b0058e2b6fdbf50ba
Johannes M. Schmitt <schmittjoh@gmail.com>
2010-11-15 18:19:46 +0100

    first draft for DI configuration

--- DependencyInjection/PaymentExtension.php
+++ DependencyInjection/PaymentExtension.php
@@ -3,18 +3,30 @@
 namespace Bundle\PaymentBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Reference;
 
 class PaymentExtension extends Extension
 {
 	public function configLoad(array $config, ContainerBuilder $container)
 	{
 		$xmlLoader = new XmlFileLoader($container, __DIR__.'/../Resources/config');
-		$xmlLoader->load('payments.xml');
+		$xmlLoader->load('payment.xml');
 		$xmlLoader->load('orm.xml');
+		
+		if (isset($config['plugins'])) {
+		    $controller = $container->getDefinition('payment.plugin_controller');
+		    
+		    foreach ((array) $config['plugins'] as $pluginName) {
+		        $controller->addMethodCall('addPlugin', array(new Reference($this->getPluginId($pluginName))));
+		    }
+		}
+	}
+	
+	protected function getPluginId($name)
+	{
+	    return 'payment.plugin.'.$name;
 	}
 	
 	public function getNamespace()
