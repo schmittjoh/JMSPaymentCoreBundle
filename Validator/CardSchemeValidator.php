34a31165e57837967d6d15d573a43b560fef263e
Tim Nagel <tim@nagel.com.au>
2012-06-27 11:17:51 +1000

    Added CardScheme validator and fixes to regexes

--- Validator/CardSchemeValidator.php
+++ Validator/CardSchemeValidator.php
@@ -39,19 +39,19 @@ class CardSchemeValidator extends ConstraintValidator
         'DINERS' => array(
             '/^(36)([0-9]{12})/',
             '/^(30[0-5])([0-9]{11})/',
-            '/^(5[45])([0-9]{14}/'
+            '/^(5[45])([0-9]{14})/'
         ),
         'DISCOVER' => array(
             '/^(6011)([0-9]{12})/',
-            '/^(64[4-9])([0-9]{13}',
-            '/^(65)([0-9]{14}/',
+            '/^(64[4-9])([0-9]{13})/',
+            '/^(65)([0-9]{14})/',
             '/^(622)(12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|91[0-9]|92[0-5])([0-9]{10})/'
         ),
         'INSTAPAYMENT' => array(
             '/^(63[7-9])([0-9]{13})/'
         ),
         'JCB' => array(
-            '/^(652[8-9]|65[3-8][0-9])([0-9]{12})/'
+            '/^(352[8-9]|35[3-8][0-9])([0-9]{12})/'
         ),
         'LASER' => array(
             '/^(6304|670[69]|6771)([0-9]{12, 15})/'
@@ -75,7 +75,7 @@ class CardSchemeValidator extends ConstraintValidator
      */
     public function validate($value, Constraint $constraint)
     {
-        if (null === $value) {
+        if (null === $value || '' === $value) {
             return;
         }
 
