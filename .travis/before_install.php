543467ab109a90fa106cffe814bc0642a9e88b99
Martin Georgiev <martin.georgiev@gmail.com>
2017-12-01 11:28:34 +0000

    Prevent Travis throwing an out of memory error on older PHP

--- .travis/before_install.php
+++ .travis/before_install.php
@@ -3,5 +3,10 @@
 
 include_once 'common.php';
 
+# Prevent Travis throwing an out of memory error on older PHP
+if (getPhpVersion() < '5.6') {
+    run('echo "memory_limit=-1" >> ~/.phpenv/versions/5.3/etc/conf.d/travis.ini');
+}
+
 // Disable XDebug
 run('phpenv config-rm xdebug.ini');
