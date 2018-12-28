276601214d35dae54b741e434dfdbd8532d7604c
Paulo Rodrigues Pinto <regularjack@gmail.com>
2018-12-28 14:12:06 +0000

    Use different routing config in functional tests for <4.0

--- Tests/Functional/TestBundle/Resources/config/framework.php
+++ Tests/Functional/TestBundle/Resources/config/framework.php
@@ -2,28 +2,34 @@
 
 use Symfony\Component\HttpKernel\Kernel;
 
-$assets = array('assets' => false);
-
-if (version_compare(Kernel::VERSION, '2.7', '<')) {
-    // The 'assets' configuration is only available for Symfony >= 2.7
-    $assets = array();
-}
-
-$container->loadFromExtension('framework', array_merge($assets, array(
-    'secret' => 'test',
-    'test' => true,
-    'session' => array(
-        'storage_id' => 'session.storage.mock_file',
-    ),
-    'templating' => array(
-        'engines' => array('twig', 'php'),
-    ),
-    'router' => array(
-        'resource' => '%kernel.root_dir%/TestBundle/Resources/config/routing.yml',
-    ),
+$config = [
     'form' => true,
-    'validation' => array(
+    'router' => [
+        'resource' => '%kernel.root_dir%/TestBundle/Resources/config/routing.yml',
+    ],
+    'secret' => 'test',
+    'session' => [
+        'storage_id' => 'session.storage.mock_file',
+    ],
+    'templating' => [
+        'engines' => ['twig', 'php'],
+    ],
+    'test' => true,
+    'validation' => [
         'enabled' => true,
         'enable_annotations' => true,
-    ),
-)));
+    ],
+];
+
+if (version_compare(Kernel::VERSION, '2.7', '>=')) {
+    // The 'assets' configuration is only available for Symfony >= 2.7
+    $config['assets'] = false;
+}
+
+if (version_compare(Kernel::VERSION, '4.0', '<')) {
+    $config['router'] = [
+        'resource' => '%kernel.root_dir%/TestBundle/Resources/config/routing_legacy.yml',
+    ];
+}
+
+$container->loadFromExtension('framework', $config);
